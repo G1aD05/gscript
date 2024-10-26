4695c95041cb2008b3efbcceac9a04170697dafe
Glados <ezrag2000@proton.me>
2024-10-25 23:01:22 -0700

    Create tokenTypes.py

---
+++ src/tokenTypes.py
@@ -0,0 +1,19 @@
+class Tokens:
+    # Keywords
+
+    PRINT = "PRINT"
+    ADD = "ADD"
+    SUB = "SUB"
+    MUL = "MUL"
+    DIV = "DIV"
+    ASSIGN = "ASSIGN"
+    REPLACE = "REPLACE"
+    USE = "USE"
+    TOSTRING = "TOSTRING"
+    TONUMBER = "TONUMBER"
+
+    # Others
+
+    ILLEGAL = "ILLEGAL"
+    EXIT = "EXIT"
+    NOTE = "//"
