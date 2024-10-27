1ba207c82cf53d8cf055ce78a180b3bdba405d17
Glados <ezrag2000@proton.me>
2024-10-27 12:26:36 -0700

    Update tokenizer.py

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -11,46 +11,50 @@ class Tokenize:
 
     def tokenize(self):
         tokens = []
-        match self.code[0]:
-            case "PRINT":
-                self.keyword = Tokens.PRINT
-            case "ADD":
-                self.keyword = Tokens.ADD
-            case "SUB":
-                self.keyword = Tokens.SUB
-            case "MUL":
-                self.keyword = Tokens.MUL
-            case "DIV":
-                self.keyword = Tokens.DIV
-            case "ASSIGN":
-                self.keyword = Tokens.ASSIGN
-            case "REPLACE":
-                self.keyword = Tokens.REPLACE
-            case "TOSTRING":
-                self.keyword = Tokens.TOSTRING
-            case "TONUMBER":
-                self.keyword = Tokens.TONUMBER
-            case "EXIT":
-                self.keyword = Tokens.EXIT
-            case "//":
-                self.keyword = Tokens.NOTE
-            case "OPEN":
-                self.keyword = Tokens.READ
-            case "WRITE":
-                self.keyword = Tokens.WRITE
-            case "INPUT":
-                self.keyword = Tokens.INPUT
-            case "DOWNLOAD":
-                self.keyword = Tokens.DOWNLOAD
-            case "JOIN":
-                self.keyword = Tokens.JOIN
-            case "RANDOM":
-                self.keyword = Tokens.RANDOM
-            case _:
-                if self.code[0] == "":
-                    pass
-                else:
+        if len(self.code) > 0:
+            match self.code[0]:
+                case "PRINT":
+                    self.keyword = Tokens.PRINT
+                case "ADD":
+                    self.keyword = Tokens.ADD
+                case "SUB":
+                    self.keyword = Tokens.SUB
+                case "MUL":
+                    self.keyword = Tokens.MUL
+                case "DIV":
+                    self.keyword = Tokens.DIV
+                case "ASSIGN":
+                    self.keyword = Tokens.ASSIGN
+                case "REPLACE":
+                    self.keyword = Tokens.REPLACE
+                case "TOSTRING":
+                    self.keyword = Tokens.TOSTRING
+                case "TONUMBER":
+                    self.keyword = Tokens.TONUMBER
+                case "EXIT":
+                    self.keyword = Tokens.EXIT
+                case "//":
+                    self.keyword = Tokens.NOTE
+                case "OPEN":
+                    self.keyword = Tokens.READ
+                case "WRITE":
+                    self.keyword = Tokens.WRITE
+                case "INPUT":
+                    self.keyword = Tokens.INPUT
+                case "DOWNLOAD":
+                    self.keyword = Tokens.DOWNLOAD
+                case "JOIN":
+                    self.keyword = Tokens.JOIN
+                case "RANDOM":
+                    self.keyword = Tokens.RANDOM
+                case "WAIT":
+                    self.keyword = Tokens.WAIT
+                case "RUN":
+                    self.keyword = Tokens.RUN
+                case _:
                     self.keyword = Tokens.ILLEGAL
+        else:
+            pass
 
         for token in range(1, len(self.code)):
             tokens.append(self.code[token])
