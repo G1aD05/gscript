85bf20980bd8d3d8fcb46f9adfa0aa7107d0e796
Glados <ezrag2000@proton.me>
2024-10-26 20:34:39 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import sys
 from tokenizer import Tokenize
 from parser import Parse
-from common import variables, line
+from common import *
 
 
 code = ""
@@ -10,13 +10,13 @@ if len(sys.argv) > 1:
     try:
         code = open(sys.argv[1], "r").read()
         for lines in code.split("\n"):
-            variables.update(Parse(Tokenize(lines).tokens).return_vars())
+            variables.update(Parse(Tokenize(lines.strip()).tokens).return_vars())
             line += 1
     except Exception as e:
         print("Exception occurred: ", str(e))
 else:
     while True:
         try:
-            variables.update(Parse(Tokenize(input(">>> ")).tokens).return_vars())
+            variables.update(Parse(Tokenize(input(">>> ").strip()).tokens).return_vars())
         except Exception as e:
             print("Exception occurred: ", str(e))
