88ca082ef3d503ae589b9fac33f271379af34d1a
Glados <ezrag2000@proton.me>
2024-10-28 18:19:06 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@
 import sys
 from tokenizer import Tokenize
 from parser import Parse
-from common import *
+from common import public
+from colorama import Fore, Style
 
 
 code = ""
@@ -10,14 +11,15 @@ if len(sys.argv) > 1:
     try:
         code = open(sys.argv[1], "r").read()
         for lines in code.split("\n"):
-            variables.update(Parse(Tokenize(lines.strip()).tokens).return_vars())
-            line += 1
+            public.variables.update(Parse(Tokenize(lines.strip()).tokens).return_vars())
+            public.line += 1
     except Exception as e:
-        print("Exception occurred: ", str(e))
+        print(Fore.RED + Style.BRIGHT + "Exception occurred: ", str(e) + Style.REST_ALL)
         exit(1)
 else:
+    console = True
     while True:
         try:
-            variables.update(Parse(Tokenize(input(">>> ").strip()).tokens).return_vars())
+            public.variables.update(Parse(Tokenize(input(">>> ").strip()).tokens).return_vars())
         except Exception as e:
-            print("Exception occurred: ", str(e))
+            print(Fore.RED + Style.BRIGHT + "Exception occurred: ", str(e) + Style.REST_ALL)
